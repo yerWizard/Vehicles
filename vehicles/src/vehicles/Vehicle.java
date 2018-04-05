7c77ab7fc5565256250bcb228b68456ad16ab98c
cfgs <cfgs@pc-info-host>
2018-04-05 20:38:32 +0200

    Commit Inicial

---
+++ vehicles/src/vehicles/Vehicle.java
@@ -0,0 +1,24 @@
+package vehicles;
+
+public class Vehicle {
+
+	private static int VELOCITAT_MAXIMA = 120;
+
+	private int cilindrada;
+	private int portes;
+	private int velocitat_actual;
+
+	public Vehicle(){
+
+		this.velocitat_actual = 0;
+	}
+
+	public void accelerar(){
+
+		if(this.velocitat_actual <= this.VELOCITAT_MAXIMA){
+			this.velocitat_actual += 10;
+		}
+
+	}
+
+}
