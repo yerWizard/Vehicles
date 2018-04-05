8cf1d799b211e592d7484b74601e7e9b6cb51ff2
yerWizard <mqlgarciaperez9@gmail.com>
2018-04-05 20:57:16 +0200

    afegir marxes

--- vehicles/src/vehicles/Vehicle.java
+++ vehicles/src/vehicles/Vehicle.java
@@ -8,9 +8,13 @@ public class Vehicle {
 	private int portes;
 	private int velocitat_actual;
 
+	private int marxes;
+	private int marxa_actual;
+
 	public Vehicle(){
 
 		this.velocitat_actual = 0;
+		this.marxes = 5;
 	}
 
 	public void accelerar(){
@@ -25,6 +29,22 @@ public class Vehicle {
 
 		if(this.velocitat_actual >= 10){
 			this.velocitat_actual -= 10;
+		}
+
+	}
+
+	public void pujaMarxa(){
+
+		if(this.marxes >= 0 && this.marxes < 5){
+			this.marxa_actual ++;
+		}
+
+	}
+
+	public void baixaMarxa(){
+
+		if(this.marxes <= 5 && this.marxes > 0){
+			this.marxa_actual --;
 		}
 
 	}
